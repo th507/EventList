931d764a06da6f98fa253abee8f14805daa7c8f4
Jingwei Liu <john.th507@gmail.com>
2013-03-09 21:37:50 +0800

    changed directory: from src to lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,46 @@
+/*jshint node:true*/
 module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['src/*.js']
+      all: ['lib/*.js']
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.main %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: 'lib/<%= pkg.main %>.js',
+        dest: 'dist/<%= pkg.main %>.min.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['lib/<%= pkg.main %>.js'], dest: 'dist/<%= pkg.main %>.js', filter: 'isFile'} // includes files in path
+        ]
       }
     }
   });
 
 
-   // Load the plugin that provides the "jshint" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks("grunt-contrib-copy");
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('closure', 'using brew closure-compiler', function() {
+    var exec = require('child_process').exec;
+    var cb = this.async();
+    exec('closure-compiler --js lib/eventlist.js --create_source_map dist/eventlist.map --source_map_format=V3 --js_output_file dist/eventlist.min.js', {cwd: './'}, function(err, stdout, stderr) {
+      console.log(stdout);
+      cb();
+    });
+  });
+
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('build', ['jshint', 'closure']);
+  grunt.registerTask('default', ['jshint', 'copy']);
 
 };
